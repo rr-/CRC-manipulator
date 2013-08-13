2b7bc50eed88bef70b1c032362e670f67ada1449
Marcin Kurczewski <mkurczew@gmail.com>
2013-08-13 10:43:36 +0200

    Better distribution script

--- distro.sh
+++ distro.sh
@@ -1,7 +1,43 @@
-VERSION=0.24
-rm -f crcmanip-$VERSION.bin.zip
-rm -f crcmanip-$VERSION.src.zip
-upx --ultra-brute gui/bin/Release/crcmanip-gui.exe
-upx --ultra-brute cli/bin/release/crcmanip.exe
-find . -type f \( -not -ipath '*bin*' -and -not -ipath '*obj*' \) | zip crcmanip-$VERSION.src.zip -@
-zip crcmanip-$VERSION.bin.zip -j cli/bin/release/crcmanip.exe gui/bin/Release/crcmanip-gui.exe README.md mit-license
+#!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#get version
+defines=$(cat "$DIR/cli/version.h" | grep '#define' | grep -v 'VERSION_H')
+eval $(echo "$defines"|sed 's/#define //;s/ /=/;')
+VERSION="$MAJOR_VERSION.$MINOR_VERSION"
+
+#start
+rm -f ./*.zip
+mkdir "$DIR/tmp"
+
+#compile gui
+pushd gui
+	msbuild=$(cygpath 'C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe')
+	"$msbuild" *.sln /t:Rebuild /verbosity:quiet /nologo /p:Configuration=Release /p:OutputPath="$(cygpath -w $DIR/tmp)"
+	rm -rf "$DIR/gui/obj"
+popd
+
+#compile cli
+pushd cli
+	gcc="g++"
+	"$gcc" {*/,}*.cpp -o "$DIR/tmp/crcmanip.exe"
+popd
+
+#compress exes
+cp mit-license "$DIR/tmp/LICENSE"
+"$DIR/tmp/crcmanip" --help>"$DIR/tmp/README"
+upx --ultra-brute "$DIR/tmp/*.exe"
+pushd "$DIR/tmp"
+	find . -type f | zip -q "$DIR/crcmanip-$VERSION.bin.zip" -@
+popd
+
+#compress sources
+rm "$DIR/tmp/"*.exe
+cp -r "$DIR/gui" "$DIR/tmp/"
+cp -r "$DIR/cli" "$DIR/tmp/"
+pushd "$DIR/tmp"
+	find . -type f -not -iname '.*.swp' | zip -q "$DIR/crcmanip-$VERSION.src.zip" -@
+popd
+
+#cleanup
+rm -rf "$DIR/tmp"
